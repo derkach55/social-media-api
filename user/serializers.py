6b319dbd8dc6ed558edff9a1be578c706b47c8ed
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-27 20:43:11 +0300

    added user serializer

---
+++ user/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from user.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'password', 'is_staff')
+        read_only_fields = ('is_staff', )
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        """Creating user with encrypted password"""
+        return User.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Updating user with correct changing of password"""
+        password = validated_data.pop('password')
+        user = super(UserSerializer, self).update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
