691c1f1e3fd4cd53677cc9d77e6eabe334128e01
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-27 20:52:36 +0300

    KeyError fixed

--- user/serializers.py
+++ user/serializers.py
@@ -16,7 +16,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         """Updating user with correct changing of password"""
-        password = validated_data.pop('password')
+        password = validated_data.pop('password', None)
         user = super(UserSerializer, self).update(instance, validated_data)
         if password:
             user.set_password(password)
