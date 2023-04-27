587a0d99d6378232ab37e4863433164cbb3b1eeb
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-27 21:03:04 +0300

    added user profile view

--- user/urls.py
+++ user/urls.py
@@ -4,12 +4,13 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from user.views import UserRegistryView
+from user.views import UserRegistryView, UserManageView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', UserRegistryView.as_view(), name='register'),
+    path('profile/', UserManageView.as_view(), name='profile'),
 
 ]
 app_name = 'user'
--- user/views.py
+++ user/views.py
@@ -1,7 +1,18 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from user.serializers import UserSerializer
 
 
 class UserRegistryView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class UserManageView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def get_object(self):
+        return self.request.user
