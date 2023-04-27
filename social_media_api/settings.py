439659a5930c6a07129865227210a66dd4f248ff
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-27 21:06:25 +0300

    changed jwt settings

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -111,6 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
