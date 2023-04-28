4ffc893fb491fdba5f565fe4f9bb209b8f7c3d6f
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-28 16:10:01 +0300

    added user list & retrieve views

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.routers import DefaultRouter
 
-from user.views import UserRegistryView, UserManageView, LogoutView
+from user.views import UserRegistryView, UserManageView, LogoutView, UserViewSet
+
+router = DefaultRouter()
+router.register('', UserViewSet)
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -12,5 +16,6 @@ urlpatterns = [
     path('register/', UserRegistryView.as_view(), name='register'),
     path('profile/', UserManageView.as_view(), name='profile'),
     path('profile/logout/', LogoutView.as_view(), name='logout'),
+    path('', include(router.urls))
 ]
 app_name = 'user'
--- user/views.py
+++ user/views.py
@@ -1,9 +1,10 @@
 from rest_framework.response import Response
-from rest_framework import generics, views, status
+from rest_framework import generics, views, status, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from user.models import User
 from user.serializers import UserSerializer
 
 
@@ -33,3 +34,9 @@ class LogoutView(views.APIView):
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
