c4d9b47c2458be2e35d6c4daff31f7ad011eae0f
Derkach Oleksandr <derkach03553@gmail.com>
2023-04-29 14:21:38 +0300

    added post model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
